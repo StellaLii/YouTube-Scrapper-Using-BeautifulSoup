50564a7884717cfd1ffa6e6612c4f3c50569376b
Himal Baral <riseabovehate.baral@gmail.com>
2017-10-21 19:40:01 +0545

    Update YTScrapper.py
    Try-Catch Exceptions added !

--- YTScrapper.py
+++ YTScrapper.py
@@ -2,28 +2,44 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
-url_list_file = pd.read_excel("urls.xlsx")
+url_list_file = pd.read_excel("urls.xlsx", chunk_size=50)
+
+for chunk_size in url_list_file:
+    print (chunk_size)
+
 
 def scrap_body(url):
     r  = requests.get(url)
     data = r.text
     soup = BeautifulSoup(data)
-    title = soup.select('.watch-title')
-    title = title[0].getText()
-    title = title.strip().replace('\n',' ')
-    views = soup.select('.watch-view-count')
-    views = views[0].getText()
-    views = views.strip().replace('views','')
-
-    watch_sentiment = soup.find_all(id = 'watch8-sentiment-actions')[0]
-    soup.find_all(id = 'watch8-sentiment-actions')[0]
-
-    button_class_like = watch_sentiment.find_all('button')[0]
-    watch_sentiment.find_all('button')[0]
-    like= button_class_like.find_all('span')[0].get_text()
-    watch_sentiment.find_all('button')[2]
-    button_class_dislike = watch_sentiment.find_all('button')[2]
-    dislike = button_class_dislike.find_all('span')[0].get_text()
+    print(url)
+    try:
+        title = soup.select('.watch-title')
+        title = title[0].getText()
+        title = title.strip().replace('\n',' ')
+    except:
+        title = ''
+    try:    
+        views = soup.select('.watch-view-count')
+        views = views[0].getText()
+        views = views.strip().replace('views','')
+    except: 
+        views= ''
+    try:
+        watch_sentiment = soup.find_all(id = 'watch8-sentiment-actions')[0]
+        soup.find_all(id = 'watch8-sentiment-actions')[0]
+        button_class_like = watch_sentiment.find_all('button')[0]
+        watch_sentiment.find_all('button')[0]
+        like= button_class_like.find_all('span')[0].get_text()
+    except: 
+        like= ''
+        
+    try:
+        watch_sentiment.find_all('button')[2]
+        button_class_dislike = watch_sentiment.find_all('button')[2]
+        dislike = button_class_dislike.find_all('span')[0].get_text()
+    except: 
+        dislike = ''
     datas = [url, title, views, dislike, like] 
     return datas
 
